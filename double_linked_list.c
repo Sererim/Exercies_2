5b9718c85389369d8bd16e1eb800a49418476b76
Sererim <danielleymanw@gmail.com>
2023-05-31 17:53:42 +0300

    Added print function.

--- double_linked_list.c
+++ double_linked_list.c
@@ -7,6 +7,28 @@ typedef struct Node{
     struct Node* prev;
 } Node;
 
+void print(Node* tail)
+{
+    printf("[ ");
+    for (Node* curr = tail; curr != NULL; curr = curr -> next)
+    {
+        printf("%d ", curr -> data);
+    }
+    printf("]");
+}
+
+void print(Node* tail, Node* head)
+{
+    Node* curr = tail;
+    Node* urr = head;
+    while (curr != NULL && urr != NULL)
+    {
+        printf("%d %d\n", curr -> data, urr -> data);
+        curr = curr -> next;
+        urr = urr -> prev;
+    }
+}
+
 int main(int argc, char* argv[])
 {
     Node* tail = malloc(sizeof(Node));
@@ -33,6 +55,10 @@ int main(int argc, char* argv[])
     tail -> next -> next -> data = 3;
     tail -> next -> next -> prev = tail -> next;
     tail -> next -> next -> next = NULL;
+    Node* head = tail -> next -> next;
+
+    print(tail);
+    print(tail, head);
 
     return 0;
 }
