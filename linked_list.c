4a6fac7c35126312a8217fcb0e46ec14fe9fe228
Sererim <danielleymanw@gmail.com>
2023-05-31 17:30:51 +0300

    Final commit. Added deallocation, push and reverse functions.

--- linked_list.c
+++ linked_list.c
@@ -6,27 +6,89 @@ typedef struct Node {
     struct Node* next;
 } Node;
 
-void print(Node root)
+void print(Node* root)
 {
     printf("List:\n[ ");
-    for(Node* current = &root; current != NULL; current = current -> next)
+    for(Node* current = root; current != NULL; current = current -> next)
     {
         printf("%d ", current -> data);
     }
     printf("]\n");
 }
 
+void push(Node** root, int val)
+{
+    Node* new_node = malloc(sizeof(Node));
+    if (new_node == NULL)
+    {
+        exit(1);
+    }
 
+    new_node -> next = NULL;
+    new_node -> data = val;
+
+    if (*root == NULL)
+    {
+        *root = new_node;
+        return;
+    }
+
+    Node* curr = *root;
+    while (curr -> next != NULL)
+    {
+        curr = curr -> next;
+    }
+    curr -> next = new_node;
+}
+
+void deallocate(Node** root)
+{
+    Node* temp = NULL;
+    for (Node* curr = *root; curr != NULL; curr = curr -> next) 
+    {
+        temp = curr;
+        free(temp);
+    }
+    *root = NULL;
+}
+
+void reverse(Node** root)
+{
+    Node* prev = NULL;
+    Node* next = NULL;
+    Node* curr = *root;
+
+    while (curr != NULL)
+    {
+        next = curr -> next;
+        curr -> next = prev;
+        
+        prev = curr;
+        curr = next;
+    }
+
+    *root = prev;
+}
 
 int main(int argc, char* argv[])
 {
-    Node root;
-    root.data = 10;
-    root.next = malloc(sizeof(Node));
-    root.next -> data = 11;
-    root.next -> next = NULL;
-
-    print(root)
-    free(root.next);
+    Node* root = malloc(sizeof(Node));
+    if (root == NULL)
+    {
+        exit(2);
+    }
+    
+    push(&root, 1);
+    push(&root, 2);
+    push(&root, 3);
+    push(&root, 4);
+    push(&root, 5);
+    push(&root, 6);
+    print(root);
+    
+    reverse(&root);
+    print(root);
+    
+    deallocate(&root);
     return 0;
 }
